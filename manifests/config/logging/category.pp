eb3974516c7584a66daeb7c3833516474be892f6
Philipp Frik <philipp.frik@noris.de>
2017-06-21 16:16:43 +0200

    bissl weiter

---
+++ manifests/config/logging/category.pp
@@ -0,0 +1,14 @@
+# define logging categorys
+define bind::config::logging::category
+(
+  $target,
+  $category = $::name,
+)
+{
+    concat::fragment { "${::category}-category":
+    target  => 'logging.conf',
+    order   => 10,
+    content => template("${::module_name}/category.erb"),
+  }
+  
+}
---
+++ manifests/config/logging/channel.pp
@@ -0,0 +1,14 @@
+# define logging channels
+define bind::config::logging::channel
+(
+  $target,
+  $channel = $::name,
+)
+{
+    concat::fragment { "${::channel}-channel":
+    target  => 'logging.conf',
+    order   => 05,
+    content => template("${::module_name}/channel.erb"),
+  }
+  
+}
