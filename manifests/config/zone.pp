3119469404b0a93f1161efff9c0760510c525c0f
Philipp Frik <philipp.frik@noris.de>
2017-06-27 15:25:09 +0200

    begin of zone definiton

---
+++ manifests/config/zone.pp
@@ -0,0 +1,17 @@
+# create zone files
+define ::bind::config::zone(
+  $type,
+  $zone_name = $::bind::config::zone::title,
+  $allow_query = undef,
+  $allow_query_on = undef,
+
+
+
+
+
+  
+)
+{
+  
+  
+}
