9e8694631269cc7f490f66224bca17d8f7874166
Philipp Frik <philipp.frik@noris.de>
2017-06-28 16:09:05 +0200

    space fix

--- manifests/config/view.pp
+++ manifests/config/view.pp
@@ -1,27 +1,22 @@
 # create view files
-define bind::config::view(
-  $view_name = $::title,
-  $match_clients = undef,
-  $match_destinations = undef,
-  $match_recursive_only = undef,
-  )
-  {
+define bind::config::view ($view_name = $::title, $match_clients = undef, $match_destinations = undef, $match_recursive_only = undef,) {
   concat { "view-${::view_name}":
-	  owner => $::bind::binduser,
-	  group => $::bind::bindgroup,
-	  mode  => '0644',
-	  path  => $::bind::configdir,
+    owner => $::bind::binduser,
+    group => $::bind::bindgroup,
+    mode  => '0644',
+    path  => $::bind::configdir,
   }
-  
+
   concat::fragment { 'View Header':
-    target => "view-${::view_name}",
-    order => 00,
+    target  => "view-${::view_name}",
+    order   => 00,
     content => '# This file ist managed by Puppet',
   }
+
   concat::fragment { 'View Footer':
-    target => "view-${::view_name}",
-    order => 00,
+    target  => "view-${::view_name}",
+    order   => 00,
     content => '} ;',
   }
-  
+
 }
